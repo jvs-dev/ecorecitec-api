5d9a4d4933951f4401935af980fbc3a33b7c6f97
jvs-dev <shepherdcom12@gmail.com>
2025-08-14 09:56:35 -0300

    test cors

--- pages/api/send-payRecibe.js
+++ pages/api/send-payRecibe.js
@@ -20,32 +20,36 @@ const transporter = nodemailer.createTransport({
 });
 
 async function init(req, res) {
-   res.setHeader('Access-Control-Allow-Origin', 'https://eco-recitec.com.br');
+   // Adicionado os headers CORS para aceitar qualquer origem
+   res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-   // Se a requisição for do tipo OPTIONS (preflight), respondemos e terminamos aqui.
-   // Isso é um passo obrigatório para que os navegadores permitam a requisição POST.
+   // Se a requisição for do tipo OPTIONS (preflight), respondemos com 200 OK e terminamos aqui.
+   // Isso permite que o navegador continue com a requisição POST.
    if (req.method === 'OPTIONS') {
       res.status(200).end();
       return;
    }
 
-   try {
-      const form = new formidable.IncomingForm();
-      const [fields, files] = await form.parse(req);
-      const comprovantePagamento = files.payRecibe[0];
-      const sheetsName = fields.sheetsName[0];
-      const nome = fields.nome[0];
-      const email = fields.email[0];
-      const telefone = fields.telefone[0];
-      const data = fields.data[0];
+   // Apenas processa a requisição se o método for POST.
+   if (req.method === 'POST') {
 
-      const mailOptions = {
-         from: process.env.EMAIL_USER,
-         to: "celene.recitec@donaverde.com.br",
-         subject: 'Nova inscrição no site da EcoRecitec!',
-         html: `
+      try {
+         const form = new formidable.IncomingForm();
+         const [fields, files] = await form.parse(req);
+         const comprovantePagamento = files.payRecibe[0];
+         const sheetsName = fields.sheetsName[0];
+         const nome = fields.nome[0];
+         const email = fields.email[0];
+         const telefone = fields.telefone[0];
+         const data = fields.data[0];
+
+         const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: "celene.recitec@donaverde.com.br",
+            subject: 'Nova inscrição no site da EcoRecitec!',
+            html: `
                     <p>Olá Celene,</p>
                     <p>Sou o bot feito para enviar as inscrições e comprovantes!</p>
                     <h2>Nova Inscrição Registrada no Evento</h2>
@@ -61,25 +65,29 @@ async function init(req, res) {
                     <br>                    
                     <img src="https://eco-recitec.com.br/images/logo/logo-recitec-02-02.png" />
                 `,
-         // Anexa o arquivo ao e-mail
-         attachments: [
-            {
-               filename: comprovantePagamento.originalFilename,
-               path: comprovantePagamento.filepath,
-               contentType: comprovantePagamento.mimetype,
-            },
-         ],
-      };
-      await transporter.sendMail(mailOptions);
+            // Anexa o arquivo ao e-mail
+            attachments: [
+               {
+                  filename: comprovantePagamento.originalFilename,
+                  path: comprovantePagamento.filepath,
+                  contentType: comprovantePagamento.mimetype,
+               },
+            ],
+         };
+         await transporter.sendMail(mailOptions);
 
-      // Remove o arquivo temporário após o envio do e-mail para evitar acúmulo de lixo
-      fs.unlinkSync(comprovantePagamento.filepath);
+         // Remove o arquivo temporário após o envio do e-mail para evitar acúmulo de lixo
+         fs.unlinkSync(comprovantePagamento.filepath);
 
-      res.status(200).json({ message: "Menssagem enviada." });
+         res.status(200).json({ message: "Menssagem enviada." });
 
-   } catch (error) {
-      console.error("Erro ao enviar e-mail:", error);
-      res.status(500).json({ error: error.message || "Erro interno do servidor ao enviar email." });
+      } catch (error) {
+         console.error("Erro ao enviar e-mail:", error);
+         res.status(500).json({ error: error.message || "Erro interno do servidor ao enviar email." });
+      }
+   } else {
+      // Se o método não for POST ou OPTIONS, retorna um erro 405.
+      res.status(405).json({ message: 'Method Not Allowed' });
    }
 };
 
